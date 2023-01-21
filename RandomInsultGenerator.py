dd0d64c5de00061de3d34f703361f51a3252f615
Conkerer <111583852+Conkerer0@users.noreply.github.com>
2023-01-21 16:15:13 +0000

    Add files via upload

---
+++ RandomInsultGenerator.py
@@ -0,0 +1,7 @@
+import random
+#random instults
+
+words1_List = ['Stinky ','Smelly ','Gross ','Fat ','Rancid ','Turgid ','Poopy ','Sweaty ','Facist ']
+words2_List = ['Awful ','Disgusting ','Upperclass ','American ','Silly ','Sussy ','Sus ','Amongletonitisy ','Capitalistic ']
+words3_List = ['Wee Wee ','Shart ','Corpse ','Grandma ','Grandad ','Poo Poo ','Amongle ','Pringleton ','Antishart ']
+print(random.choice(words1_List)+random.choice(words2_List)+random.choice(words3_List))
